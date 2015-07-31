5b24d91156ae5cf3c4702839f914f15c7517bc6a
shyakaster <shyakaster@gmail.com>
2015-07-31 11:26:40 +0300

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'signup', :to=>'users#new'
+
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ user.rb
@@ -0,0 +1,8 @@
+class Greeting
+  def good_morning(name)
+    result= "Good morning #{name.capitalize}"
+    return result
+  end
+end
+
+
